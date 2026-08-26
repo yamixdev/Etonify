38c60444d34909d8ca90567e4ef422ec12ccaa3f
YamixDEV <bkru2281@gmail.com>
2026-08-26 12:30:23 +0300

    fix(android): avoid conflicting ABI filters

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,9 +37,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        ndk {
-            abiFilters += setOf("armeabi-v7a", "arm64-v8a")
-        }
     }
 
     signingConfigs {
